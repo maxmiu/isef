e6372e22ffb8da247df04ef7ab6cbf36f1a7465f
maxmiu <maximilian.moeller1@iubh-fernstudium.de>
2022-06-15 23:40:19 +0200

    #4: Use heroku SourceVersion to get git hash

--- scripts/write-hash.js
+++ scripts/write-hash.js
@@ -1,10 +1,9 @@
 const fs = require("fs");
-const childProcess = require("child_process");
 
 const versionPath = "./web/src/version.ts";
 
-const gitHash = childProcess.execSync("git rev-parse --short HEAD").toString().trim();
+const hash = process.env.SOURCE_VERSION;
 const dummyVersion = fs.readFileSync(versionPath);
-const updatedVersion = dummyVersion.toString().replace(/dummyHash/g, gitHash);
+const updatedVersion = dummyVersion.toString().replace(/dummyHash/g, hash);
 
 fs.writeFileSync(versionPath, updatedVersion);
