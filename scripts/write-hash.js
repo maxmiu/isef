3fa3fe078fd789e4969bb1a2ae35ecbdf7c00b5b
maxmiu <maximilian.moeller1@iubh-fernstudium.de>
2022-06-15 23:28:19 +0200

    #4: Cleanup build

--- scripts/write-hash.js
+++ scripts/write-hash.js
@@ -3,7 +3,6 @@ const childProcess = require("child_process");
 
 const versionPath = "./web/src/version.ts";
 
-childProcess.execSync("git config --global --add safe.directory /github/workspace");
 const gitHash = childProcess.execSync("git rev-parse --short HEAD").toString().trim();
 const dummyVersion = fs.readFileSync(versionPath);
 const updatedVersion = dummyVersion.toString().replace(/dummyHash/g, gitHash);
